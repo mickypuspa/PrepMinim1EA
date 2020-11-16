682d1ba3a8f1d4914d2ba34de85afb44a8382ee9
Miquel Puspa Torres Campo <miqueltorrescampo@gmail.com>
2020-11-16 13:09:39 +0100

    'BackendInProgress'

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+// init
+const app = express();
+
+// settings
+
+app.set('port', process.env.PORT || 3000);
+
+// middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+// routes
+
+app.get('/', (req,res) => {
+    return res.send(`The API is at http://localhost:${app.get('port')}`);
+});
+
+export default app;
