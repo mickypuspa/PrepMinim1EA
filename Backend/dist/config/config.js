e08bd258220aff66b2f9b7b6c1322a3844d968ac
Miquel Puspa Torres Campo <miqueltorrescampo@gmail.com>
2020-11-17 11:50:53 +0100

    'nuevoFrontend'

--- Backend/dist/config/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    DB: {
-        URI: process.env.MONGODB_URI || 'mongodb://localhost/pruebamin1',
-        USER: process.env.MONGODB_USER,
-        PASSWORD: process.env.MONGODB_PASSWORD
-    }
-};
